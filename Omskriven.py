de88d95c05e5b221ff904c855e94c6299e128941
elviramoberg <elviram@chalmers.se>
2023-05-11 09:33:57 +0200

    hej

--- Omskriven.py
+++ Omskriven.py
@@ -13,20 +13,27 @@ t_int = 0.1     # time interval
 t_f = 30        # final time
 
 
-def kinetic_energy(v_x, v_y, m):
+# def kinetic_energy(v_x, v_y, m):
+#     K = m/2*((v_x)**2+(v_y)**2)
+#     return K
+#
+#
+# def potential_energy(x, y, k):
+#     r = np.sqrt(x**2+y**2)
+#     U = -k/r
+#     return U
+#
+#
+# def total_energy(K, U):
+#     E = K + U
+#     return E
+
+def energy(x, y, v_x, v_y, m, k):
     K = m/2*((v_x)**2+(v_y)**2)
-    return K
-
-
-def potential_energy(x, y, k):
     r = np.sqrt(x**2+y**2)
     U = -k/r
-    return U
-
-
-def total_energy(K, U):
     E = K + U
-    return E
+    return K, U, E
 
 
 def angular_momentum(x, y, v_x, v_y, m):
@@ -63,27 +70,33 @@ def plot_trajectories(x0, y0, v_x0, v_y0_values, t_f, t_int):
         plt.title("Object Trajectory")
         plt.xlabel('x')
         plt.ylabel('y')
-        plt.xlim([-3, 2])
-        plt.ylim([-2, 2])
+        # plt.xlim([-3, 2])
+        # plt.ylim([-2, 2])
         plt.axhline(y=0, color='gray', linewidth=0.5)
         plt.axvline(x=0, color='gray', linewidth=0.5)
         plt.legend()
         plt.show()
 
 
-def calculate_energy_components(x0, y0, vx0, vy0, t_f, t_int):
+# def calculate_energy_components(x0, y0, vx0, vy0, t_f, t_int):
+#     diff_sol = calculate_solution(x0, y0, vx0, vy0, t_f, t_int)
+#
+#     K = kinetic_energy(diff_sol.y[2], diff_sol.y[3], m)     # skickar in x- och y-hastigheterna som vektorer
+#     U = potential_energy(diff_sol.y[0], diff_sol.y[1], k)   # skickar in x- och y-positionerna som vektorer
+#     E = total_energy(K, U)
+#
+#     return diff_sol.t, K, U, E
+
+
+def calculate_energy_components(x0, y0, vx0, vy0, t_f, t_int, m, k):
     diff_sol = calculate_solution(x0, y0, vx0, vy0, t_f, t_int)
-
-    K = kinetic_energy(diff_sol.y[2], diff_sol.y[3], m)     # skickar in x- och y-hastigheterna som vektorer
-    U = potential_energy(diff_sol.y[0], diff_sol.y[1], k)   # skickar in x- och y-positionerna som vektorer
-    E = total_energy(K, U)
-
+    K, U, E = energy(diff_sol.y[0], diff_sol.y[1], diff_sol.y[2], diff_sol.y[3], m, k)
     return diff_sol.t, K, U, E
 
 
 def plot_energy_components(x0, y0, vx0, vy0_values, t_f, t_int):
     for vy0 in vy0_values:
-        t, K, U, E = calculate_energy_components(x0, y0, vx0, vy0, t_f, t_int)
+        t, K, U, E = calculate_energy_components(x0, y0, vx0, vy0, t_f, t_int, m, k)
         plt.plot(t, E, label=f'E(t) for vy0 = {round(float(vy0),1)}')
         plt.plot(t, U, label=f'U(t) for vy0 = {round(float(vy0),1)}')
         plt.plot(t, K, label=f'K(t) for vy0 = {round(float(vy0),1)}')
@@ -98,10 +111,7 @@ def plot_energy_components(x0, y0, vx0, vy0_values, t_f, t_int):
 
 
 def calculate_angular_momentum(x0, y0, vx0, vy0, t_f, t_int):
-    t_span = (0, t_f)
-    z0 = np.array([x0, y0, vx0, vy0])
-    diff_sol = solve_ivp(equations_of_motion, t_span, z0, t_eval=[i * t_int for i in range(int(t_f / t_int) + 1)], max_step=0.01)
-
+    diff_sol = calculate_solution(x0, y0, vx0, vy0, t_f, t_int)
     L = angular_momentum(diff_sol.y[0], diff_sol.y[1], diff_sol.y[2], diff_sol.y[3], m)
     return diff_sol.t, L
 
@@ -128,6 +138,3 @@ plot_energy_components(x0, y0, v_x0, v_y0_values, t_f, t_int)
 
 # Plotting time evolution of angular momentum
 plot_angular_momentum(x0, y0, v_x0, v_y0_values, t_f, t_int)
-
-
-
