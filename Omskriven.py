9533814cdfdef52a8b46bfc6b29dfefabf819004
elviramoberg <elviram@chalmers.se>
2023-05-11 12:08:57 +0200

    hej

--- Omskriven.py
+++ Omskriven.py
@@ -7,26 +7,11 @@ from scipy.integrate import solve_ivp
 k = 1
 m = 1
 x0, y0, v_x0 = 1, 0, 0
-v_y0_values = [0.5, 1, 1.2, np.sqrt(2), 2]
+v_y0_values = [0.5, 1, np.sqrt(2), 2]
 
-t_int = 0.1     # time interval
+t_int = 0.01     # time interval
 t_f = 30        # final time
 
-
-# def kinetic_energy(v_x, v_y, m):
-#     K = m/2*((v_x)**2+(v_y)**2)
-#     return K
-#
-#
-# def potential_energy(x, y, k):
-#     r = np.sqrt(x**2+y**2)
-#     U = -k/r
-#     return U
-#
-#
-# def total_energy(K, U):
-#     E = K + U
-#     return E
 
 def energy(x, y, v_x, v_y, m, k):
     K = m/2*((v_x)**2+(v_y)**2)
@@ -53,13 +38,13 @@ def equations_of_motion(t, z):
 def calculate_solution(x0, y0, v_x0, v_y0, t_f, t_int):
     t_span = (0, t_f)
     z0 = np.array([x0, y0, v_x0, v_y0])
-    diff_sol = solve_ivp(equations_of_motion, t_span, z0, t_eval=[i*t_int for i in range(int(t_f/t_int)+1)], max_step=0.01)
+    diff_sol = solve_ivp(equations_of_motion, t_span, z0, t_eval=np.linspace(0, t_f, int(t_f/t_int)+1), max_step=0.01, atol=1e-6)
     return diff_sol
 
 
 def calculate_trajectory(x0, y0, v_x0, v_y0, t_f, t_int):
     diff_sol = calculate_solution(x0, y0, v_x0, v_y0, t_f, t_int)
-    return diff_sol.y[0], diff_sol.y[1]     # returnerar vektorer med x- och y-positionerna av banan
+    return diff_sol.y[0], diff_sol.y[1]
 
 
 def plot_trajectories(x0, y0, v_x0, v_y0_values, t_f, t_int):
@@ -70,22 +55,10 @@ def plot_trajectories(x0, y0, v_x0, v_y0_values, t_f, t_int):
         plt.title("Object Trajectory")
         plt.xlabel('x')
         plt.ylabel('y')
-        # plt.xlim([-3, 2])
-        # plt.ylim([-2, 2])
         plt.axhline(y=0, color='gray', linewidth=0.5)
         plt.axvline(x=0, color='gray', linewidth=0.5)
         plt.legend()
         plt.show()
-
-
-# def calculate_energy_components(x0, y0, vx0, vy0, t_f, t_int):
-#     diff_sol = calculate_solution(x0, y0, vx0, vy0, t_f, t_int)
-#
-#     K = kinetic_energy(diff_sol.y[2], diff_sol.y[3], m)     # skickar in x- och y-hastigheterna som vektorer
-#     U = potential_energy(diff_sol.y[0], diff_sol.y[1], k)   # skickar in x- och y-positionerna som vektorer
-#     E = total_energy(K, U)
-#
-#     return diff_sol.t, K, U, E
 
 
 def calculate_energy_components(x0, y0, vx0, vy0, t_f, t_int, m, k):
